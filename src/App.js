2d1a108781bed2aa22931f832d187615c5a29e43
ShwetSd <119885670+shwetd19@users.noreply.github.com>
2023-05-06 11:44:22 +0530

    updated the page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import Popup from './Popup';
-import './App.css';
+import React, { useState } from "react";
+import Popup from "./Popup";
+import "./App.css";
 
 function App() {
   return (
